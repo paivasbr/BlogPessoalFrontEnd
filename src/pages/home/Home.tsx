a960a2b5de4b83d5910feba8cbc93799dc49ae63
Brenda <contatobrendapaiva@outlook.com>
2022-02-17 17:21:27 -0300

    Blog create project

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1>Minjae Lee <em>“Greno”</em></h1>
+        <img src="https://imgur.com/akdukF4.png" alt="Imagem Tela Inicial" width="1349" height="900"/>
+        </>
+    );
+}
+
+export default Home;

55781d5ded7f9094fd8778bd02395bcdf596b3b6
Brenda <contatobrendapaiva@outlook.com>
2022-02-22 01:22:54 -0300

    Router

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-// from '@material-ui/core';
 import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a).</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Conheça as maravilhosas lendas da Amazônia!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo1">Seja bem vindo(a).</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo2">Conheça as maravilhosas lendas da Amazônia!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className="botao">Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://imgur.com/kVf7r99.png" alt="" width="685px" height="479px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className="postagens">
                 </Grid>
             </Grid>
         </>

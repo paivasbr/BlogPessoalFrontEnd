020714043740d1568edd91491d3cbc58feee0ddc
Brenda <contatobrendapaiva@outlook.com>
2022-02-22 22:59:19 -0300

    Security

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { api } from '../../services/Service';
+import UserRegister from '../userRegister/UserRegister';
 import './Login.css';
 
 function Login() {
@@ -14,11 +17,9 @@ function Login() {
                         <TextField id='Usuario(a)' label='Usuario(a)' variant='outlined' name='Usuario(a)' margin='normal' fullWidth />
                         <TextField id='Senha' label='Senha' variant='outlined' name='Senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary' className='button2'>
                                     Logar
                                 </Button>
-                            </Link>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>

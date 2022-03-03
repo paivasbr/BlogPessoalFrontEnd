d00da80783005098434f0d771064dbc23e8b6586
Brenda <contatobrendapaiva@outlook.com>
2022-03-03 14:26:41 -0300

    Redux

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import {Typography, Box, Grid, Button} from '@material-ui/core';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
-import useLocalStorage from 'react-use-localstorage';
 import { useHistory } from 'react-router-dom';
 import './Home.css';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../store/tokens/TokensReducer';
 
 function Home() {
     let history = useHistory();
-    const [token, seToken] = useLocalStorage('token');
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
 
     useEffect(() => {
         if(token == "") {

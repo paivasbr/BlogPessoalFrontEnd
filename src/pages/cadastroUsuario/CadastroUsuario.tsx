d2971e1f5fa718d00b0fd592d599039831e52906
Brenda <contatobrendapaiva@outlook.com>
2022-03-06 00:45:21 -0300

    Atualização

--- src/pages/cadastroUsuario/CadastroUsuario.tsx
+++ src/pages/cadastroUsuario/CadastroUsuario.tsx
@@ -20,62 +20,62 @@ function CadastroUsuario() {
         id: 0, nome: "", usuario: "", senha: ""
     })
 
-        useEffect(()=>{
-            if(userResult.id !== 0){
-                history.push('/login')
-                
-            }
-        }, [userResult])
-        
-        function confirmacaoDeSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
-            setconfirmacaoDeSenha(e.target.value)
-        }
-        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
-            setUser({
-                ...user,
-                [e.target.name]: e.target.value
-            })
-        }
+    useEffect(() => {
+        if (userResult.id !== 0) {
+            history.push('/login')
 
-        async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-            e.preventDefault()
-            if (confirmacaoDeSenha ===  user.senha && user.senha.length >= 8) {
-                cadastroUsuario(`usuarios/cadastrar`, user, setUserResult)
-                toast.success('Usuário(a) cadastrado(a) com sucesso!', {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    theme: "colored",
-                    progress: undefined,
-                });
-            } else {
-                toast.error('Dados inconsistentes. Favor verificar as informações de cadastro.', {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    theme: "colored",
-                    progress: undefined,
-                });
-            }
         }
+    }, [userResult])
+
+    function confirmacaoDeSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
+        setconfirmacaoDeSenha(e.target.value)
+    }
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if (confirmacaoDeSenha === user.senha && user.senha.length >= 8) {
+            cadastroUsuario(`usuarios/cadastrar`, user, setUserResult)
+            toast.success('Usuário(a) cadastrado(a) com sucesso!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        } else {
+            toast.error('Dados inconsistentes. Favor verificar as informações de cadastro.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        }
+    }
 
 
     return (
-            <Grid container direction='row'>
+        <Grid container direction='row'>
             <Grid className='container_cadastro'>
                 <Box className='card_cadastro'>
-                    <form onSubmit={onSubmit}>
-                        <Typography variant='h3' component='h3' className='texto_cadastro'>Cadastrar</Typography>
+                    <form onSubmit={onSubmit} >
+                        <Typography  variant='h3' component='h3' className='texto_cadastro'>Cadastrar</Typography>
                         <TextField value={user.nome}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='Nome' variant='outlined' name='nome' margin='normal' fullWidth />
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='Nome' variant='outlined' name='nome' margin='normal'  fullWidth />
                         <TextField value={user.usuario}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuário(a)' variant='outlined' name='usuario' margin='normal' fullWidth />
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='E-mail' variant='outlined' name='usuario' margin='normal' fullWidth />
                         <TextField value={user.senha}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <TextField value={confirmacaoDeSenha}
@@ -86,14 +86,14 @@ function CadastroUsuario() {
                                     Cancelar
                                 </Button>
                             </Link>
-                                <Button type='submit' variant='contained' className="button-submit buttton2">
-                                    Cadastrar
-                                </Button>  
+                            <Button type='submit' variant='contained' className="button-submit buttton2">
+                                Cadastrar
+                            </Button>
                         </Box>
                     </form>
                 </Box>
             </Grid>
-        
+
         </Grid>
     );
 }
